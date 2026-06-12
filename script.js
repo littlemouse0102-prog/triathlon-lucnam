18010cc1b7f9bbf49e148c67e7d1ca668b549e80
littlemouse0102-prog <littlemouse0102@gmail.com>
2026-06-12 09:50:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+const csvUrl =
+"https://docs.google.com/spreadsheets/d/1AWZRIlJTNjbVUdLm4X-oItdpyBzfO6kq/export?format=csv&gid=20758593";
+
+fetch(csvUrl)
+  .then(response => response.text())
+  .then(data => {
+    console.log(data);
+
+    const rows = data.trim().split("\n");
+
+    rows.forEach(row => {
+      console.log(row.split(","));
+    });
+  })
+  .catch(error => console.error(error));
