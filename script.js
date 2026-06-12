c18446a50e2b53f07e9eb7d071aea98affb2fd3d
littlemouse0102-prog <littlemouse0102@gmail.com>
2026-06-12 10:07:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const csvUrl =
 "https://docs.google.com/spreadsheets/d/1AWZRIlJTNjbVUdLm4X-oItdpyBzfO6kq/export?format=csv&gid=20758593";
 
 fetch(csvUrl)
-  .then(response => response.text())
-  .then(data => {
-    console.log(data);
-
-    const rows = data.trim().split("\n");
-
-    rows.forEach(row => {
-      console.log(row.split(","));
-    });
+  .then(response => {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `<p>Response: ${response.status}</p>`
+    );
+    return response.text();
   })
-  .catch(error => console.error(error));
+  .then(data => {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `<pre>${data.substring(0, 500)}</pre>`
+    );
+  })
+  .catch(error => {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `<p>Lỗi: ${error}</p>`
+    );
+  });
